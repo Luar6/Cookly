6d89743187e4326f708cad177e98f0876140efee
Lucas Costa Marques <124885548+LucasCostaMrq@users.noreply.github.com>
2025-02-02 15:05:40 -0300

    Integração do projeto
    As views em cadastro.ejs e login.ejs do projeto em express foram integradas ao projeto frontend da tela inicial em React 
    OBS: entrar em ambas as pastas e executar "npm install", após isso, ainda em ambas as pastas, executar "npm start"

--- src/app/main/nav/Nav.jsx
+++ src/app/main/nav/Nav.jsx
@@ -4,9 +4,9 @@ import './Nav.css'
 export default props => 
     <nav className="navbar">  
         <ul className="navbar-nav">
-            <li className="nav-item"><a href="" className="nav-link">Home</a></li>
-            <li className="nav-item"><a href="" className="nav-link">Sign In</a></li>
-            <li className="nav-item"><a href="" className="nav-link">Sign Up</a></li>
+            <li className="nav-item"><a href="./" className="nav-link">Home</a></li>
+            <li className="nav-item"><a href="http://localhost:5000/login" className="nav-link">Sign In</a></li>
+            <li className="nav-item"><a href="http://localhost:5000/cadastro" className="nav-link">Sign Up</a></li>
         </ul>   
     </nav>
     
